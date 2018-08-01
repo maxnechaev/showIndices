907fbfb7a7bb3c2e4e0ffb8a81164623d38d1520
Maxim Nechaev <4max.nechaev@gmail.com>
2018-08-01 21:15:17 +0000

    Final version

--- showIndices.js
+++ showIndices.js
@@ -1,17 +1,33 @@
 var str = String(process.argv.slice(2));
 var myObject = {};
 
-function countLetters(str) {
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] == " " || str[i] === ",") {
+function showIndices() {
+  for (var j = 0; j < str.length; j++) {
+    if (str[j] == " " || str[j] === ",") {
       //check for empty spaces and commas
-    } else if (myObject[str[i]] == undefined) {
-      myObject[str[i]] = 1;
     } else {
-      myObject[str[i]] += 1;
+      var indices = [];
+      myObject[str[j]] = indices;
+      var letterToIndex = str[j];
+      for (var i = 0; i < str.length; i++) {
+        if (str[i] == letterToIndex)
+          indices.push(i);
+      }
     }
   }
   return myObject;
 }
 
-console.log(countLetters(str));
+console.log(showIndices(str));
+
+// The function should return all the indices (zero-based positions) in the string
+// where each character is found. So for each letter, we are returning
+// (potentially) multiple numbers to represent all the places in the string that it shows up.
+//
+// hello will result in
+// {
+// h: [0],
+// e: [1],
+// l: [2, 3],
+// o: [4]
+// }
